e7be86a9fc43bce366d59d0ec7742e7e79c05562
seungyeopme <seungyeoop.me.dev@gmail.com>
2020-06-30 18:00:55 +0900

    Java 2020.06.30(2)

--- Test01/src/study6.java
+++ Test01/src/study6.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class study6 {
 
@@ -293,11 +293,349 @@ public class study6 {
 			} 
 			System.out.println(Arrays.toString(scores[i])); }
 		*/
+		
+		/*
+		String str[]= {"A","B","C"};
+		for(int i=0; i<3;i++) {
+			if(str[i]=="A") {
+				break;  // for문 탈출! 반복문 탈출!
+			}
+			System.out.println(str[i]);	//출력 안됨
+		}
+		*/
+		
+		/*
+		//1
+		//2
+		//3
+		//4
+		//5
+		//6
+		//1.0
+		//2.0
+		//3.0
+		//4.0
+		//5.0
+		//6.0
+		double ary[][]=new double[2][3];
+		// 실수형 2행 3열
+		Scanner s = new Scanner(System.in);
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<3; j++) {
+				ary[i][j]= s.nextDouble();
+				// 실수를 배열(2행 3열)에다 입력
+			}
 			
+		}
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<3; j++) {
+				System.out.println(ary[i][j]);
+			}
+		}
+		*/
+		
+		/*
+		int arr[][] = new int[2][3];
+        int a, i, j;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("2차원 배열안의 값을 순차적으로 입력하세요");
+        // arr의 전체길이
+        for(i=0; i<arr.length; i++) {
+            // arr[i]의 전체 
+            for(j=0; j<arr[i].length; j++) {
+                System.out.print("arr[" + i + "][" + j + "] : ");
+                a = sc.nextInt();
+                arr[i][j] = a;
+            }
+        }
+        for(i=0; i<2; i++) {
+            for(j=0; j<3; j++) {
+                System.out.println("arr[" + i + "][" + j + "] : " + arr[i][j]);
+            }
+        }
+		*/
+	
+		/*
+//		aa[0][0]부터 aa[2][3]까지 출력 
+//
+//		  1   2   3   4 
+//		  5   6   7   8 
+//		  9  10  11  12 
 
+		int[][]aa=new int[3][4];
+		int i, k;
+		int val =1;
+		
+		for(i=0; i<3; i++) {
+			for(k=0; k<4; k++) {
+				aa[i][k] = val;
+				val++;
+			}
+		}
+		
+		System.out.println("aa[0][0]부터 aa[2][3]까지 출력 \n");
+		
+		for(i=0; i<3; i++) {
+			for(k=0; k<4; k++) {
+				System.out.printf("%3d ", aa[i][k]);
+			}
+			System.out.printf("\n");
+		}
+		*/
 	
 		
+/*		
+//		aa[0][0]부터 aa[3][5]까지 출력 
+//
+//		  2   4   6   8  10 
+//		 12  14  16  18  20 
+//		 22  24  26  28  30 
+
 		
+		int[][]aa=new int[3][5];
+		int i, k;
+		int val =2;
+		
+		for(i=0; i<3; i++) {
+			for(k=0; k<5; k++) {
+				aa[i][k] = val;
+				val+=2;
+			}
+		}
+		
+		System.out.println("aa[0][0]부터 aa[3][5]까지 출력 \n");
+		
+		for(i=0; i<3; i++) {
+			for(k=0; k<5; k++) {
+				System.out.printf("%3d ", aa[i][k]);
+			}
+			System.out.printf("\n");
+		}
+*/
+		/*
+//		1	2	3	4	5	
+//		6	7	8	9	10	
+//		11	12	13	14	15	
+//		16	17	18	19	20	
+
+		int ary[][]= new int[4][5];
+		int n=1;
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<5; j++) {
+				ary[i][j]=n++;
+				System.out.print(ary[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		*/
+		
+/*		
+//		  1*  1=  1  2*  1=  2  3*  1=  3  4*  1=  4  5*  1=  5  6*  1=  6  7*  1=  7  8*  1=  8  9*  1=  9
+//		  1*  2=  2  2*  2=  4  3*  2=  6  4*  2=  8  5*  2= 10  6*  2= 12  7*  2= 14  8*  2= 16  9*  2= 18
+//		  1*  3=  3  2*  3=  6  3*  3=  9  4*  3= 12  5*  3= 15  6*  3= 18  7*  3= 21  8*  3= 24  9*  3= 27
+//		  1*  4=  4  2*  4=  8  3*  4= 12  4*  4= 16  5*  4= 20  6*  4= 24  7*  4= 28  8*  4= 32  9*  4= 36
+//		  1*  5=  5  2*  5= 10  3*  5= 15  4*  5= 20  5*  5= 25  6*  5= 30  7*  5= 35  8*  5= 40  9*  5= 45
+//		  1*  6=  6  2*  6= 12  3*  6= 18  4*  6= 24  5*  6= 30  6*  6= 36  7*  6= 42  8*  6= 48  9*  6= 54
+//		  1*  7=  7  2*  7= 14  3*  7= 21  4*  7= 28  5*  7= 35  6*  7= 42  7*  7= 49  8*  7= 56  9*  7= 63
+//		  1*  8=  8  2*  8= 16  3*  8= 24  4*  8= 32  5*  8= 40  6*  8= 48  7*  8= 56  8*  8= 64  9*  8= 72
+//		  1*  9=  9  2*  9= 18  3*  9= 27  4*  9= 36  5*  9= 45  6*  9= 54  7*  9= 63  8*  9= 72  9*  9= 81
+
+		// i=0 j=0 결과 1	
+		// i=0 j=1 결과 2 
+		// i=1 j=0 결과 2
+		// i=1 j=1 결과 4
+		
+		int[][]ary=new int[9][9];
+		
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				ary[i][j] = (i+1)*(j+1);
+			}
+		}
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				System.out.printf("%3d*%3d=%3d", j+1, i+1, ary[i][j]);
+			}
+			System.out.println();
+		}
+*/
+	
+/*
+//		1	2	
+//		3	4	
+//		5	6	
+		//3행 2열
+		int ary[][] = new int[][]{
+								  {1,2},
+								  {3,4},
+								  {5,6}
+								 };
+		int n=1;
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<2; j++) {
+			ary[i][j]=n++;
+			System.out.print(ary[i][j]+"\t");
+			}
+			System.out.println();
+		}
+*/
+/*		
+//		1	5	
+//		2	6	
+//		3	7	
+//		4	8	
+
+		int ary[][] = {	//2행 4열
+						{1,2,3,4},
+						{5,6,7,8}
+			 		  };
+		int ary1[][] = new int[4][2];//ary의 데이터를 ary1에 복사 (위치가 다름) 4행 2열
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<4; j++) {
+				ary1[j][i]=ary[i][j];
+			}
+	}
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<2;j++) {
+				System.out.printf(ary1[i][j]+"\t");
+				
+			}
+			System.out.println();
+		}
+*/
+/*	
+//		1
+//		2
+//		3
+//		4
+//		5
+//		6
+//		21
+
+		// [2][3] 정수형 2차원 배열
+		// 값을 입력받아 총합
+		int sum=0;
+		Scanner s = new Scanner(System.in);
+		int ary[][] = new int[2][3];
+		for(int i=0; i<ary.length; i++) {
+			for(int j=0; j<ary[i].length; j++) {
+				ary[i][j] = s.nextInt();
+				sum=sum+ary[i][j];
+			}
+		}
+		System.out.println(sum);
+*/
+		
+/*
+		// 2차원 배열일때
+		// 행 -> 배열명.length
+		// 열 -> 배열명[행].length
+		
+		String s[][] = {{"java"},
+						{"c","c++"},
+						{"html","css","python"}};
+		
+		for(int i=0; i<s.length; i++) {
+			for(int j=0; j<s[i].length; j++) {	//s[i].length  행을 기준으로 열이 몇 개인지; length 열 부분 처리할 때는 행 기준으로!!! 행은 고정적인데 열이 가변적일 때 사용을 한다!!!
+				System.out.println(s[i][j]);
+			}
+		}
+*/
+		
+/*
+		String str="오늘은 화요일";
+		System.out.println(str.length()); //7
+		System.out.println(str.charAt(1)); //늘
+*/		
+		
+/*		
+//		오늘은 화요일
+//		공부하고 티비봄!!
+
+		String str="오늘은 화요일,공부하고 티비봄!!";
+		String a[]=str.split(",");
+		for(String b:a) {	// for each문
+			System.out.println(b);
+		}
+*/
+/*
+		String str="오늘은 화요일,공부하고 티비봄!!";
+		String a=str.substring(4, 7);
+		System.out.println(a);			//화요일
+*/
+/*
+		String str="오늘은 화요일,공부하고 티비봄!!";
+		String b=str.replace("공부", "study");
+		System.out.println(b); 			//오늘은 화요일,study하고 티비봄!!
+*/
+/*		
+		String str="오늘은 화요일";
+		String str1=str.concat("내일은 수요일");
+		//문자열 연결
+		System.out.println(str1);
+*/
+/*	
+//		문자열 입력 ==> IT CookBook Java 입니다
+//		출력 문자열 ==> IT C$$kB$$k Java 입니다
+		Scanner s = new Scanner(System.in);
+		String str;
+		
+		System.out.print("문자열 입력 ==> ");
+		str = s.nextLine();
+		
+		System.out.print("출력 문자열 ==> ");
+		for (int i=0; i< str.length(); i++) {
+			if(str.charAt(i)== 'o')
+				System.out.printf("%c", '$');
+			else
+				System.out.printf("%c", str.charAt(i));
+		}
+*/
+		
+		/*
+		// 2개의 과일이름을 입력받아(영문)
+		// 문자열이 긴 단어를 출력
+		Scanner s = new Scanner(System.in);
+		String a=s.next();
+		String b=s.next();
+		
+		if(a.length() < b.length())
+			System.out.println(b);
+		else
+			System.out.println(a);
+		*/
+		
+		/*
+		String a="apple";
+		String b="banana";
+		
+		if(a.compareTo(b)>0) {	//a>b
+			System.out.println(a);
+		}
+		else if(a.compareTo(b)<0) {	//a<b
+			// b가 a보다 나중에 나옴
+			System.out.println(b);
+		}
+		else	//a.compareTo(b)==0, 0이면 같은 문자열
+			System.out.println("같은 문자열!");
+		*/
+		
+		/*
+//		원 문자열1 ==> [Java Programming]
+//		원 문자열2 ==> [Java IT CookBook]
+//		7
+//		true
+
+		String str1 = "Java Programming";
+		String str2 = "Java IT CookBook";
+		
+		System.out.println("원 문자열1 ==> [" + str1 + "]");
+		System.out.println("원 문자열2 ==> [" + str2 + "]");
+		
+		System.out.println(str1.compareTo(str2));	// 두 문자열 비교
+		System.out.println(str1.contains("Java"));	// "Java" 글자가 포함되었는지 확인
+		*/
 		
 	}
 }
